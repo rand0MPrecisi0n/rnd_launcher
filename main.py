a3e165530588677b23bfedade45907dc29e9476e
rand0MPrecisi0n <mauibandeira@gmail.com>
2016-10-07 22:43:58 -0300

    Update main.py
    included authenticators

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Includes database functions, quality selector and web-parsing
 #print(a.dict)
 
 import functions as functions
-import sys, shelve
+import sys, shelve, subprocess, webbrowser
 
 #User selection
 shelf = shelve.open("default.shlf", writeback=True)
@@ -20,18 +20,25 @@ except KeyError:
 
 #OAuth
 try:
-    shelf["oauth"]
-    oauth = shelf["oauth"]
+    shelf["oauth_rnd"]
+    oauth_rnd = shelf["oauth_rnd"]
+    shelf["oauth_ls"]
+    oauth_ls = shelf["oauth_ls"]
 except KeyError:
-    oauth = input("Please paste your OAUTH token:")
-    shelf["oauth"] = oauth
+    subprocess.Popen("livestreamer --twitch-oauth-authenticate", shell=True)
+    webbrowser.open(r"https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=57escm9wy6fayv570mj99rx0147mn4q&redirect_uri=http://localhost&scope=user_read+user_follows_edit+user_subscriptions+chat_login")
+    token_rnd = input("Please paste token for rnd_launcher (first page)")
+    token_ls = input("Please paste token for livestreamer (second page)")
+    shelf["oauth_rnd"] = token_rnd
+    shelf["oauth_ls"] = token_ls
+    oauth_rnd = shelf["oauth_rnd"]; oauth_ls = shelf["oauth_ls"]
 shelf.close()
 
 #Create instance
-instance = functions.Db(user, oauth)
+instance = functions.Db(user, oauth_rnd, oauth_ls)
 
 #options dispatch
-options = {"h":instance.help, "help":instance.help, "list":instance.display_db, "clear":instance.clear_db, "chat":instance.chat_settings, "quit":sys.exit, "default":instance.default, "oauth":instance.oauth_token}
+options = {"h":instance.help, "help":instance.help, "list":instance.display_db, "clear":instance.clear_db, "chat":instance.chat_settings, "quit":sys.exit, "default":instance.default, "show oauth":instance.show_oauth, "live":instance.live}
 qualities = {"1":"source", "2":"high", "3":"medium", "4":"low", "5":"mobile"}
 
 def launch_stream():
@@ -40,7 +47,7 @@ def launch_stream():
     instance.launcher(stream, quality)
 
 #Intro
-print("{0:*^60}\n{1:*^60}\n{2:*^60}\n".format(" Hello "+user.upper()+" ", " Welcome to Twitch Launcher ", " At any point, use option \"h\" for help "))
+print("{0:*^60}\n{1:*^60}\n{2:*^60}\n".format(" Hello "+user.upper()+" ", " Welcome to rnD Launcher ", " At any point, use option \"h\" for help "))
 
 #Control Flow
 
